30b6c1ecf1c892178dc1d39e974cbdcbfe3a83ab
Lucas Ercolano <lercolano@udesa.edu.ar>
2023-08-11 16:22:57 -0300

    dividi en modulos

---
+++ Main.hs
@@ -0,0 +1,11 @@
+import Point
+-----------------
+import City
+-----------------
+import Quality
+-------------------
+import Link
+-------------------
+import Tunel
+-------------------
+import Region
