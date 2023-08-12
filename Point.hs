1be8d76b0ded09060a9137d635fd39bbec8a4284
Lucas Ercolano <lercolano@udesa.edu.ar>
2023-08-12 09:09:39 -0300

    hice quality y agregue ejemplos
    era cortito, hice un par de variables en ambos

--- Point.hs
+++ Point.hs
@@ -2,8 +2,15 @@ module Point (Point, newP, difP) where
 
 data Point = Poi Int Int deriving (Eq, Show)
 
-newP :: Int -> Int -> Point  
-newP x y = Poi x y
+newP :: Int -> Int -> Point
+newP = Poi
 
 difP :: Point -> Point -> Float
 difP (Poi x1 y1) (Poi x2 y2) = sqrt (fromIntegral ((x1 - x2)^2 + (y1 - y2)^2))
+
+
+origin :: Point
+origin = newP 0 0
+
+distance :: Float
+distance = difP origin (newP 3 4)
--- Quality.hs
+++ Quality.hs
@@ -1,10 +1,30 @@
---module Quality ( Quality, newQ, capacityQ, delayQ )
-module Quality ( Quality)--, newQ, capacityQ, delayQ )
-where
-
+module Quality (Quality, newQ, capacityQ, delayQ) where
 
 data Quality = Qua String Int Float deriving (Eq, Show)
 
---newQ :: String -> Int -> Float -> Quality
---capacityQ :: Quality -> Int -- cuantos túneles puede tolerar esta conexión
---delayQ :: Quality -> Float  -- la demora por unidad de distancia que sucede en las conexiones de este canal
+newQ :: String -> Int -> Float -> Quality
+newQ = Qua
+
+capacityQ :: Quality -> Int
+capacityQ (Qua _ capacity _) = capacity
+
+delayQ :: Quality -> Float
+delayQ (Qua _ _ delay) = delay
+
+
+basicQuality :: Quality
+basicQuality = newQ "Basic" 10 0.5
+
+basicCapacity :: Int
+basicCapacity = capacityQ basicQuality
+basicDelay :: Float
+basicDelay = delayQ basicQuality
+
+
+premiumQuality :: Quality
+premiumQuality = newQ "Premium" 100 0.1
+
+areEqual :: Bool
+areEqual = basicQuality == premiumQuality
+
+
