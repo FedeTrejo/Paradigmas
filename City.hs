513ff7bf8bcfe07b18e9be991877a3d45f5ddd5e
Lucas Ercolano <lercolano@udesa.edu.ar>
2023-08-11 22:00:30 -0300

    conecte los modulos
    tuve que comentar mucho, xq al estar definido el tipo de las funciones., si no esta la funcion falla

--- City.hs
+++ City.hs
@@ -1,8 +1,11 @@
-module City ( City, newC, nameC, distanceC )
+--module City ( City, newC, nameC, distanceC )
+module City ( City)--, newC, nameC, distanceC )
+
    where
-      
+
+import Point
 data City = Cit String Point deriving (Eq, Show)
 
-newC :: String -> Point -> City
-nameC :: City -> String
-distanceC :: City -> City -> Float
+--newC :: String -> Point -> City
+--nameC :: City -> String
+--distanceC :: City -> City -> Float
