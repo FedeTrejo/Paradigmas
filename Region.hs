de02dacb99417f9502831a8c04655954a02e249a
Federico Trejo <cfedetrejo@gmail.com>
2023-08-15 11:37:44 -0300

    implementacion de funciones de region salvo tunelR

--- Region.hs
+++ Region.hs
@@ -25,15 +25,31 @@ linkR (Reg cities links tunnels) c1 c2 q =
   Reg cities (newL c1 c2 q:links) tunnels
 
 --tunelR :: Region -> [ City ] -> Region
+--ESPERANDO POR LA CORRECCION DE LINKR PARA IMPLEMENTARLO CORRECTAMENTE
 
 connectedR :: Region -> City -> City -> Bool
 connectedR (Reg _ links _) c1 c2 =
   any (linksL c1 c2) links -- @fede La función any en Haskell permite verificar si algún elemento de una lista cumple cierta condición.
 
 
---linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
---delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
---availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
+linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
+linkedR (Reg cities [] tunnels) c1 c2 = False
+linkedR (Reg cities (l:links) tunnels) c1 c2 =  if (linksL c1 c2 l) then True
+                                                else linkedR (Reg cities links tunnels) c1 c2
+
+delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+delayR (Reg cities [] tunnels) _ _ = error "No links between those cities in the region"
+delayR (Reg cities (l:links) tunnels) c1 c2 =
+  if linksL c1 c2 l
+    then delayL l
+    else delayR (Reg cities links tunnels) c1 c2
+
+availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
+availableCapacityForR (Reg cities [] _) _ _ = error "No links between those cities in the region"
+availableCapacityForR (Reg cities (l:links) tunnels) c1 c2 = 
+  if linksL c1 c2 l
+    then capacityL l
+    else availableCapacityForR (Reg cities links tunnels) c1 c2
 
 
 regBA :: Region
@@ -44,4 +60,15 @@ linkBaSt = linkR regBA bsAs stgo midQ
 
 conRBaSt :: Bool
 conRBaSt = connectedR linkBaSt bsAs stgo
+
+baStlinked :: Bool
+baStlinked = linkedR linkBaSt bsAs stgo
+
+delayBaSt :: Float
+delayBaSt = delayR linkBaSt bsAs stgo
+
+capacityBaSt :: Int
+capacityBaSt = availableCapacityForR linkBaSt bsAs stgo
+
+
 
