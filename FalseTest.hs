632c30930d597881b18fbb718658378da3d4eeff
Lucas Ercolano <lercolano@udesa.edu.ar>
2023-08-18 13:37:15 -0300

    cambie el formato al q el profe quiere

--- FalseTest.hs
+++ FalseTest.hs
@@ -5,22 +5,15 @@ import Test
 import Region
 import Tunel
 
-linNoCord :: Bool
-linNoCord = linksL stgo cordoba linBsSt
+fT = [
+  linksL stgo cordoba linBsSt,
 
-cordNotInLink :: Bool
-cordNotInLink = connectsL cordoba linBsSt  
+  connectsL cordoba linBsSt,
 
-tunBs = newT [linBsSt]
+  connectsT cordoba bsAs (newT [linBsSt]),
 
-cordNotInTun :: Bool 
-cordNotInTun = connectsT cordoba bsAs tunBs
+  usesT linkCoSg (newT [linBsSt]),
+   
+  connectedR argentina bsAs stgo,
 
-tunNotUseLink :: Bool
-tunNotUseLink = usesT linkCoSg tunBs
-
-stgoNotConn :: Bool
-stgoNotConn = connectedR argentina bsAs stgo
-
-stgoNotLinked :: Bool
-stgoNotLinked = linkedR argentina bsAs stgo
+  linkedR argentina bsAs stgo]
