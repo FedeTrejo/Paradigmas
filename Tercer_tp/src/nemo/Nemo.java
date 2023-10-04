e536fa9156f435dc30dec5476ed8ecb9dea1d0b6
Federico Trejo <cfedetrejo@gmail.com>
2023-10-04 10:41:40 -0300

    primeros tres test

--- Tercer_tp/src/nemo/Nemo.java
+++ Tercer_tp/src/nemo/Nemo.java
@@ -20,8 +20,63 @@ public class Nemo {
     public static Integer getCoordY() {
         return coords.get(1);
     }
+    public static Integer getDepth() {
+        return depth;
+    }
 
+    private static void moveLeft(){
+        if (direction == "north"){
+            direction = "west";
+        }
+        else if (direction == "west"){
+            direction = "south";
+        }
+        else if (direction == "south"){
+            direction = "east";
+        }
+        else if (direction == "east"){
+            direction = "north";
+        }
+    }
+
+    private static void moveRight(){
+        if (direction == "north"){
+            direction = "east";
+        }
+        else if (direction == "east"){
+            direction = "south";
+        }
+        else if (direction == "south"){
+            direction = "west";
+        }
+        else if (direction == "west"){
+            direction = "north";
+        }
+    }
+
+    private static void descend(){
+        depth -= 1;
+    }
+    private static void undescend(){
+        if (depth == 0){
+            depth = 0;
+        }
+        else {
+            depth += 1;
+        }
+    }
     public Nemo sendCommand(String command){
+        for(char caracter : command.toCharArray()){
+            if (caracter == 'd'){
+                descend();
+            }
+            else if (caracter == 'u'){
+                undescend();
+            }
+            else if (caracter == 'l'){}
+            else if (caracter == 'r'){}
+            else if (caracter == 'm'){}
+        }
         return this;
     }
 
--- Tercer_tp/src/nemo/Nemo_test.java
+++ Tercer_tp/src/nemo/Nemo_test.java
@@ -9,15 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Nemo_Test {
     @Test void isEverythingSetUp() {
-        assertEquals( new Nemo().getCoordX(), 0);
+        assertEquals(new Nemo().getCoordX(), 0);
         assertEquals(new Nemo().getCoordY(), 0);
         assertEquals(new Nemo().depth, 0);
         assertEquals(new Nemo().direction, "north");
     }
 
-    @Test void doNothingWithAnEmptyCommand(){
+    @Test void doNothingWithAnEmptyCommand() {
         Nemo nemo = new Nemo();
         assertEquals(nemo.sendCommand(""), nemo);
+    }
+    @Test void descendWithACommand(){
+        Nemo nemo = new Nemo();
+        assertEquals(nemo.sendCommand("d").getDepth(), -1);
     }
 
 }
