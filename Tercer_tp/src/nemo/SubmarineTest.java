32b26722d4b1149061ac5b69c42f4179fd37598c
Federico Trejo <cfedetrejo@gmail.com>
2023-10-20 12:03:59 -0300

    Tests factorizados

--- Tercer_tp/src/nemo/SubmarineTest.java
+++ Tercer_tp/src/nemo/SubmarineTest.java
@@ -1,245 +1,167 @@
 package nemo;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
+import org.junit.jupiter.api.function.Executable;
 
 public class SubmarineTest {
 
     @Test
     public void testSubmarinePosition() {
-        SubmarinePosition position = new SubmarinePosition(0, 0);
-        assertEquals(0, position.getX());
-        assertEquals(0, position.getY());
+        assertEquals(0, new SubmarinePosition(0, 0).getX());
+        assertEquals(0, new SubmarinePosition(0, 0).getY());
     }
 
     @Test
-    public void testSubmarineOrientation() {
-        North north = new North();
-        assertTrue(north.rotateLeft() instanceof West);
-        assertTrue(north.rotateRight() instanceof East);
+    public void testSubmarineNorthOrientation() {
+        assertTrue(new North().rotateLeft() instanceof West);
+        assertTrue(new North().rotateRight() instanceof East);
+    }
+    @Test
+    public void testSubmarineSouthOrientation() {
+        assertTrue(new South().rotateLeft() instanceof East);
+        assertTrue(new South().rotateRight() instanceof West);
+    }
+    @Test
+    public void testSubmarineEastOrientation() {
+        assertTrue(new East().rotateLeft() instanceof North);
+        assertTrue(new East().rotateRight() instanceof South);
+    }
+    @Test
+    public void testSubmarineWestOrientation() {
+        assertTrue(new West().rotateLeft() instanceof South);
+        assertTrue(new West().rotateRight() instanceof North);
     }
 
-    @Test public void testSubmarineDepth() {
-        SurfaceLevel surface = new SurfaceLevel();
-        assertEquals(0, surface.submarineDepths());
-
-        SafeDepthLevel safe = new SafeDepthLevel();
-        assertEquals(1, safe.submarineDepths());
-
-        UnsafeDepthLevel unsafe = new UnsafeDepthLevel();
-        assertEquals(2, unsafe.submarineDepths());
+    @Test public void testSubmarineDepths() {
+        assertEquals(0, new SurfaceLevel().submarineDepths());
+        assertEquals(1, new SafeDepthLevel().submarineDepths());
+        assertEquals(2, new UnsafeDepthLevel().submarineDepths());
     }
 
     @Test public void testSubmarineForwardCommand(){
-        Command forward = new Command("f", sub -> {
-        });
-        assertEquals("f", forward.getName());
+        assertEquals("f", new Command("f", sub -> {
+        }).getName());
     }
     @Test public void testSubmarineLeftCommand(){
-        Command left = new Command("l", sub -> {
-        });
-        assertEquals("l", left.getName());
+        assertEquals("l", new Command("l", sub -> {
+        }).getName());
     }
     @Test public void testSubmarineRightCommand(){
-        Command right = new Command("r", sub -> {
-        });
-        assertEquals("r", right.getName());
+        assertEquals("r", new Command("r", sub -> {
+        }).getName());
     }
     @Test public void testSubmarineUpCommand(){
-        Command up = new Command("u", sub -> {
-        });
-        assertEquals("u", up.getName());
+        assertEquals("u", new Command("u", sub -> {
+        }).getName());
     }
     @Test public void testSubmarineDownCommand(){
-        Command down = new Command("d", sub -> {
-        });
-        assertEquals("d", down.getName());
+        assertEquals("d", new Command("d", sub -> {
+        }).getName());
     }
     @Test public void testSubmarineShootCommand(){
-        Command shoot = new Command("m", sub -> {
-        });
-        assertEquals("m", shoot.getName());
+        assertEquals("m", new Command("m", sub -> {
+        }).getName());
     }
 
     @Test public void testIsEverythingSetUp(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        assertEquals(0, nemo.getSubmarinePosition().getX());
-        assertEquals(0, nemo.getSubmarinePosition().getY());
-        assertEquals(0, nemo.getSubmarineDepth().submarineDepths());
-        assertTrue(nemo.getSubmarineOrientation() instanceof North);
+        IsEverythingAsStart(Nemo());
     }
 
     @Test public void testEmptyCommand(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        nemo.runCommands("");
+        IsEverythingAsStart(NemoWithCommand(""));
     }
 
     @Test public void testDescendCommandAtSurface(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        nemo.runCommands("d");
-        assertEquals(1, nemo.getSubmarineDepth().submarineDepths());
+        assertEquals(1, NemoWithCommand("d").getSubmarineDepth().submarineDepths());
     }
 
     @Test public void testUndescendCommandAtSurface(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        nemo.runCommands("u");
-        assertEquals(0, nemo.getSubmarineDepth().submarineDepths());
+        assertEquals(0, NemoWithCommand("u").getSubmarineDepth().submarineDepths());
     }
     @Test public void testMoveLeftCommandAtSurface(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        nemo.runCommands("l");
-        assertTrue(nemo.getSubmarineOrientation() instanceof West);
+        assertTrue(NemoWithCommand("l").getSubmarineOrientation() instanceof West);
     }
     @Test public void testMoveRightCommandAtSurface(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        nemo.runCommands("r");
-        assertTrue(nemo.getSubmarineOrientation() instanceof East);
+        assertTrue(NemoWithCommand("r").getSubmarineOrientation() instanceof East);
     }
     @Test public void testMoveForwardCommandAtSurface(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        nemo.runCommands("f");
-        assertEquals(0, nemo.getSubmarinePosition().getX());
-        assertEquals(1, nemo.getSubmarinePosition().getY());
+        WentForward(NemoWithCommand("f"));
     }
-    @Test public void testUseAllCommandsAtSurface(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        nemo.runCommands("flrudm");
-        assertEquals(0, nemo.getSubmarinePosition().getX());
-        assertEquals(1, nemo.getSubmarinePosition().getY());
-        assertTrue(nemo.getSubmarineOrientation() instanceof North);
-        assertEquals(1, nemo.getSubmarineDepth().submarineDepths());
 
+    @Test public void testUseAllCommandsAtSurface(){
+        UsedAllCommands(NemoWithCommand("flrudm"));
     }
     @Test public void testDescendCommandUnderWater(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-        nemo.getSubmarineDepth().descend(nemo);
-        nemo.runCommands("d");
-        assertEquals(2, nemo.getSubmarineDepth().submarineDepths());
+        assertEquals(2, NemoWithCommand("dd").getSubmarineDepth().submarineDepths());
     }
     @Test public void testUndescendCommandUnderWater(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-
-        nemo.getSubmarineDepth().descend(nemo);
-        nemo.runCommands("u");
-        assertEquals(0, nemo.getSubmarineDepth().submarineDepths());
+        assertEquals(0, NemoWithCommand("du").getSubmarineDepth().submarineDepths());
     }
     @Test public void testMoveLeftCommandUnderWater(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-
-        nemo.getSubmarineDepth().descend(nemo);
-        nemo.runCommands("l");
-        assertTrue(nemo.getSubmarineOrientation() instanceof West);
+        assertTrue(NemoWithCommand("ul").getSubmarineOrientation() instanceof West);
     }
     @Test public void testMoveRightCommandUnderWater(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-
-        nemo.getSubmarineDepth().descend(nemo);
-        nemo.runCommands("r");
-        assertTrue(nemo.getSubmarineOrientation() instanceof East);
+        assertTrue(NemoWithCommand("ur").getSubmarineOrientation() instanceof East);
     }
     @Test public void testMoveForwardCommandUnderWater(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-
-        nemo.getSubmarineDepth().descend(nemo);
-        nemo.runCommands("f");
-        assertEquals(0, nemo.getSubmarinePosition().getX());
-        assertEquals(1, nemo.getSubmarinePosition().getY());
+        WentForward(NemoWithCommand("uf"));
     }
     @Test public void testUseAllCommandsUnderWater(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-
-        nemo.getSubmarineDepth().descend(nemo);
-        nemo.runCommands("flrudm");
-        assertEquals(0, nemo.getSubmarinePosition().getX());
-        assertEquals(1, nemo.getSubmarinePosition().getY());
-        assertTrue(nemo.getSubmarineOrientation() instanceof North);
-        assertEquals(1, nemo.getSubmarineDepth().submarineDepths());
+        UsedAllCommands(NemoWithCommand("dflrudm"));
     }
 
     @Test public void testShootCapsuleAtSurface(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-
-        nemo.runCommands("m");
-        assertEquals(0, nemo.getSubmarinePosition().getX());
-        assertEquals(0, nemo.getSubmarinePosition().getY());
-        assertTrue(nemo.getSubmarineOrientation() instanceof North);
-        assertEquals(0, nemo.getSubmarineDepth().submarineDepths());
+        IsEverythingAsStart(NemoWithCommand("m"));
     }
 
     @Test public void testShootCapsuleAtSafeDepthLevel(){
-        Submarine nemo = new Submarine(
-                new SubmarinePosition(0, 0),
-                new North()
-        );
-
-        nemo.getSubmarineDepth().descend(nemo);
-        nemo.runCommands("m");
-        assertEquals(0, nemo.getSubmarinePosition().getX());
-        assertEquals(0, nemo.getSubmarinePosition().getY());
-        assertTrue(nemo.getSubmarineOrientation() instanceof North);
+        Submarine nemo = NemoWithCommand("dm");
+        ArePositionAndOrientationAtTheirInitialStates(nemo);
         assertEquals(1, nemo.getSubmarineDepth().submarineDepths());
     }
 
     @Test public void testShootCapsuleAtUnsafeDepthLevel(){
-        Submarine nemo = new Submarine(
+        assertThrowsLike(()-> NemoWithCommand("dd").runCommands("m"), UnsafeDepthLevel.LAUNCH_ERROR_MESSAGE);
+    }
+
+    private static Submarine Nemo() {
+        return new Submarine(
                 new SubmarinePosition(0, 0),
                 new North()
         );
+    }
+    private static Submarine NemoWithCommand(String s) {
+        Submarine nemo = Nemo();
+        nemo.runCommands(s);
+        return nemo;
+    }
 
-        nemo.getSubmarineDepth().descend(nemo);
-        nemo.getSubmarineDepth().descend(nemo);
-        assertThrows(Exception.class, () -> {
-            nemo.runCommands("m");
-        });
+    private static void WentForward(Submarine nemo) {
+        assertEquals(0, nemo.getSubmarinePosition().getX());
+        assertEquals(1, nemo.getSubmarinePosition().getY());
+    }
+
+    private static void UsedAllCommands(Submarine nemo) {
+        WentForward(nemo);
+        assertTrue(nemo.getSubmarineOrientation() instanceof North);
+        assertEquals(1, nemo.getSubmarineDepth().submarineDepths());
+    }
+    private static void ArePositionAndOrientationAtTheirInitialStates(Submarine nemo) {
         assertEquals(0, nemo.getSubmarinePosition().getX());
         assertEquals(0, nemo.getSubmarinePosition().getY());
         assertTrue(nemo.getSubmarineOrientation() instanceof North);
-        assertEquals(2, nemo.getSubmarineDepth().submarineDepths());
+    }
+    private static void IsEverythingAsStart(Submarine nemo) {
+        ArePositionAndOrientationAtTheirInitialStates(nemo);
+        assertEquals(0, nemo.getSubmarineDepth().submarineDepths());
+    }
+    private void assertThrowsLike(Executable executable, String message ) {
+        assertEquals( message,
+                assertThrows( Exception.class, executable )
+                        .getMessage() );
     }
 }
