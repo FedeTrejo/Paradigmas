2661ccfcdcb398faa2db6f2e31f08f585478a42b
Federico Trejo <cfedetrejo@gmail.com>
2023-11-01 10:18:36 -0300

    Tablero es list de list, desarrollo de nuevos test

--- Cuarto_tp/src/linea/Conecta4.java
+++ Cuarto_tp/src/linea/Conecta4.java
@@ -1,4 +1,7 @@
 package linea;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class Conecta4 {
 
@@ -8,86 +11,98 @@ public class Conecta4 {
     private static final char BLUE = 'b';
     private static final char RED = 'r';
 
-    private final char[][] TABLE;
+    private final List<List<Character>> TABLE;
     private final int HEIGHT;
     private final int WIDTH;
+
+    private boolean itsRedTurn;
 
     public Conecta4(int rows, int cols) {
 
         this.HEIGHT = rows;
         this.WIDTH = cols;
 
-        this.TABLE = new char[rows][cols];
-
-
-        for (int i = 0; i < this.HEIGHT; i++) {
-            for (int j = 0; j < this.WIDTH; j++) {
-                this.TABLE[i][j] = notMarked;
+        this.TABLE = new ArrayList<>(rows);
+        for (int i = 0; i < rows; i++) {
+            List<Character> row = new ArrayList<>(cols);
+            for (int j = 0; j < cols; j++) {
+                row.add(notMarked);
             }
+            this.TABLE.add(row);
         }
-
+        itsRedTurn = true;
     }
 
     public String showBoard() {
-
         StringBuilder board = new StringBuilder();
 
         for (int i = 0; i < this.HEIGHT; i++) {
             board.append("|");
             for (int j = 0; j < this.WIDTH; j++) {
-                board.append(this.TABLE[i][j]);
+                board.append(this.TABLE.get(i).get(j));
             }
             board.append("|\n");
         }
 
         return board.toString();
-
     }
 
 
     public boolean finished() {
-        return fourInARow();
+        return fourInARow() || boardIsFull();
     }
-
-    public void playBlueAt(int column) {
-
-        int row = checkRowForColumn(column);
-
-        this.TABLE[row][column] = BLUE;
-
-
-    }
-
-
-    public void playRedAt(int column) {
-        int row = checkRowForColumn(column);
-
-        this.TABLE[row][column] = RED;
-
-    }
-
-    private int checkRowForColumn(int column) {
-
-        for (int row = this.HEIGHT - 1; row >= 0; row--) {
-            if (this.TABLE[row][column] == notMarked) {
-                return row;
-            }
-        }
-
-        throw new RuntimeException(columnFull);
-    }
-
-
 
     public boolean fourInARow() {
         return fourInAHorizontalRow() || fourInAVerticalRow() || fourInADiagonalRow();
     }
 
-    private boolean fourInADiagonalRow() {
+    public boolean boardIsFull() {
+        for (int i = 0; i < this.HEIGHT; i++) {
+            for (int j = 0; j < this.WIDTH; j++) {
+                if (this.TABLE.get(i).get(j) == notMarked) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+    public void playBlueAt(int column) {
+        if(!itsRedTurn && !finished()) {
+            int row = checkRowForColumn(column);
+            this.TABLE.get(row).set(column, BLUE);
+            itsRedTurn = true;
+        }else {
+            throw new RuntimeException("It's not turn or game finished");
+        }
+    }
+
+    public void playRedAt(int column) {
+        if(itsRedTurn && !finished()) {
+            int row = checkRowForColumn(column);
+            this.TABLE.get(row).set(column, RED);
+            itsRedTurn = false;
+        }else {
+            throw new RuntimeException("It's not turn or game finished");
+        }
+    }
+
+    private int checkRowForColumn(int column) {
+        for (int row = this.HEIGHT - 1; row >= 0; row--) {
+            if (this.TABLE.get(row).get(column) == notMarked) {
+                return row;
+            }
+        }
+        throw new RuntimeException(columnFull);
+    }
+
+    private boolean fourInADiagonalRow() {
         for (int i = 0; i < this.HEIGHT - 3; i++) {
             for (int j = 0; j < this.WIDTH - 3; j++) {
-                if (this.TABLE[i][j] != notMarked && this.TABLE[i][j] == this.TABLE[i + 1][j + 1] && this.TABLE[i][j] == this.TABLE[i + 2][j + 2] && this.TABLE[i][j] == this.TABLE[i + 3][j + 3]) {
+                if (this.TABLE.get(i).get(j) != notMarked &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 1).get(j + 1) &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 2).get(j + 2) &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 3).get(j + 3)) {
                     return true;
                 }
             }
@@ -95,32 +110,38 @@ public class Conecta4 {
 
         for (int i = 0; i < this.HEIGHT - 3; i++) {
             for (int j = 3; j < this.WIDTH; j++) {
-                if (this.TABLE[i][j] != notMarked && this.TABLE[i][j] == this.TABLE[i + 1][j - 1] && this.TABLE[i][j] == this.TABLE[i + 2][j - 2] && this.TABLE[i][j] == this.TABLE[i + 3][j - 3]) {
+                if (this.TABLE.get(i).get(j) != notMarked &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 1).get(j - 1) &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 2).get(j - 2) &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 3).get(j - 3)) {
                     return true;
                 }
             }
         }
-
         return false;
     }
 
     private boolean fourInAVerticalRow() {
         for (int i = 0; i < this.HEIGHT - 3; i++) {
             for (int j = 0; j < this.WIDTH; j++) {
-                if (this.TABLE[i][j] != notMarked && this.TABLE[i][j] == this.TABLE[i + 1][j] && this.TABLE[i][j] == this.TABLE[i + 2][j] && this.TABLE[i][j] == this.TABLE[i + 3][j]) {
+                if (this.TABLE.get(i).get(j) != notMarked &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 1).get(j) &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 2).get(j) &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i + 3).get(j)) {
                     return true;
                 }
             }
         }
         return false;
     }
-
-
 
     private boolean fourInAHorizontalRow() {
         for (int i = 0; i < this.HEIGHT; i++) {
             for (int j = 0; j < this.WIDTH - 3; j++) {
-                if (this.TABLE[i][j] != notMarked && this.TABLE[i][j] == this.TABLE[i][j + 1] && this.TABLE[i][j] == this.TABLE[i][j + 2] && this.TABLE[i][j] == this.TABLE[i][j + 3]) {
+                if (this.TABLE.get(i).get(j) != notMarked &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i).get(j + 1) &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i).get(j + 2) &&
+                        this.TABLE.get(i).get(j) == this.TABLE.get(i).get(j + 3)) {
                     return true;
                 }
             }
@@ -128,5 +149,7 @@ public class Conecta4 {
         return false;
     }
 
-
+    public boolean isRedTurn() {
+        return itsRedTurn;
+    }
 }
--- Cuarto_tp/src/linea/Conecta4Test.java
+++ Cuarto_tp/src/linea/Conecta4Test.java
@@ -7,41 +7,124 @@ import static org.junit.Assert.*;
 public class Conecta4Test {
 
     @Test
-    public void ShowCorrectly() {
+    public void testGameIsShownCorrectly() {
         Conecta4 game1 = new Conecta4(4, 4);
 
-
-
         game1.playRedAt(1);
-
         game1.playBlueAt(2);
 
         System.out.println( game1.showBoard() );
 
     }
 
-    @Test
-    public void GameFinalizes() {
+    @Test public void testRedStartsTheGame(){
+        Conecta4 game1 = new Conecta4(4, 4);
+        assertTrue(game1.isRedTurn());
+    }
+
+    @Test public void testOnceRedPlaysIfRedPlaysAgainShouldFail(){
+        Conecta4 game1 = new Conecta4(4, 4);
+
+        game1.playRedAt(1);
+        assertThrows(RuntimeException.class, () -> game1.playRedAt(1));
+    }
+
+    @Test public void testGameFinishesWhenFourInAHorizontalRow(){
         Conecta4 game1 = new Conecta4(4, 4);
 
         game1.playRedAt(0);
-
+        game1.playBlueAt(0);
         game1.playRedAt(1);
         game1.playBlueAt(1);
-
         game1.playRedAt(2);
         game1.playBlueAt(2);
-
         game1.playRedAt(3);
-        game1.playBlueAt(3);
-
 
         System.out.println( game1.showBoard() );
         assertTrue(game1.finished());
-
+        assertTrue(game1.fourInARow());
     }
 
+    @Test public void testGameFinishesWhenFourInAVerticalRow(){
+        Conecta4 game1 = new Conecta4(4, 4);
 
+        game1.playRedAt(0);
+        game1.playBlueAt(1);
+        game1.playRedAt(0);
+        game1.playBlueAt(1);
+        game1.playRedAt(0);
+        game1.playBlueAt(1);
+        game1.playRedAt(0);
 
-
+        System.out.println( game1.showBoard() );
+        assertTrue(game1.finished());
+        assertTrue(game1.fourInARow());
     }
+
+    @Test public void testGameFinishesWhenFourInADiagonalRow(){
+        Conecta4 game1 = new Conecta4(4, 4);
+
+        game1.playRedAt(0);
+        game1.playBlueAt(1);
+        game1.playRedAt(1);
+        game1.playBlueAt(2);
+        game1.playRedAt(2);
+        game1.playBlueAt(3);
+        game1.playRedAt(2);
+        game1.playBlueAt(3);
+        game1.playRedAt(0);
+        game1.playBlueAt(3);
+        game1.playRedAt(3);
+
+        System.out.println( game1.showBoard() );
+        assertTrue(game1.finished());
+        assertTrue(game1.fourInARow());
+    }
+
+    @Test
+    public void testGameFinishesAtTieIfNobodyWinsAndTheBoardIsFull() {
+        Conecta4 game = new Conecta4(4, 4);
+
+        game.playRedAt(0);
+        game.playBlueAt(1);
+        game.playRedAt(0);
+        game.playBlueAt(1);
+        game.playRedAt(1);
+        game.playBlueAt(0);
+        game.playRedAt(1);
+        game.playBlueAt(0);
+        game.playRedAt(2);
+        game.playBlueAt(3);
+        game.playRedAt(2);
+        game.playBlueAt(3);
+        game.playRedAt(3);
+        game.playBlueAt(2);
+        game.playRedAt(3);
+        game.playBlueAt(2);
+
+        System.out.println( game.showBoard() );
+        assertTrue(game.finished());
+        assertFalse(game.fourInARow());
+        assertTrue(game.boardIsFull());
+    }
+
+    @Test public void testOnceTheGameIsFinishedPlayingAgainShouldFail(){
+        Conecta4 game1 = new Conecta4(4, 4);
+
+        game1.playRedAt(0);
+        game1.playBlueAt(1);
+        game1.playRedAt(1);
+        game1.playBlueAt(2);
+        game1.playRedAt(2);
+        game1.playBlueAt(3);
+        game1.playRedAt(2);
+        game1.playBlueAt(3);
+        game1.playRedAt(0);
+        game1.playBlueAt(3);
+        game1.playRedAt(3);
+
+        System.out.println( game1.showBoard() );
+        assertTrue(game1.finished());
+        assertThrows(RuntimeException.class, () -> game1.playBlueAt(0));
+    }
+}
