3a3de50acb69f01719b883e530191742096f84a9
Lucas Ercolano <lercolano@udesa.edu.ar>
2023-11-12 20:26:23 -0300

    more tests

--- Cuarto_tp/src/linea/Conecta4Tests.java
+++ Cuarto_tp/src/linea/Conecta4Tests.java
@@ -1,7 +1,6 @@
 package linea;
 
-import org.junit.After;
-import org.junit.Before;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,25 +19,51 @@ public class Conecta4Tests {
         System.out.println(game.showBoard());
     }
     @Test
+    public void testShowEmptyBoard() {
+        assertEquals( emptyBoard(), game.showBoard() );
+    }
+    @Test void testLineaGameDoesNotFinishAfterANonWinningPlay() {
+        game.playRedAt(1);
+        assertFalse(game.finished());
+    }
+    @Test
     public void testCannotInitializeBoardSmallerThan4x4() {
         assertThrowsLike(() -> new Conecta4(3, 3, 'C'), Conecta4.MinBoardSize);
     }
+    @Test
+    public void testGameCannotBeInitializedWithInvalidStrategy() {
+        assertThrows(IllegalArgumentException.class, () -> new Conecta4(4, 4, 'K'));
 
+    }
     @Test public void testRedStartsTheGame(){
         assertThrowsLike(() -> game.playBlueAt(1), RedsPlay.notBlueTurn);
     }
 
+
+    @Test
+    public void testBluePlaysAfterRedTurn() {
+
+        game.playRedAt(1);
+        game.playBlueAt(1);
+        assertFalse(game.finished());
+    }
     @Test public void testOnceRedPlaysIfRedPlaysAgainShouldFail(){
         game.playRedAt(1);
         assertThrowsLike(() -> game.playRedAt(1), BluesPlay.notRedTurn);
+    }
+    @Test public void testBluePlayngInRedTurnShouldFail(){
+        assertThrowsLike(() -> game.playBlueAt(1), RedsPlay.notBlueTurn);
     }
 
     @Test public void testPuttingAPieceInAnInvalidColumnShouldFail(){
         assertThrowsLike(() -> game.playRedAt(5), Conecta4.outOfBounds);
     }
 
-    @Test public void testBluePlayngInRedTurnShouldFail(){
-        assertThrowsLike(() -> game.playBlueAt(1), RedsPlay.notBlueTurn);
+    @Test
+    public void testPuttingAPieceInAFullColumnShouldFail(){
+        playAlternatively(game, new int[]{1,1,1,1});
+        assertThrowsLike(() -> game.playRedAt(1), Conecta4.columnFull);
+        assertFalse(game.finished());
     }
 
     @Test public void testGameFinishesWhenFourInAHorizontalRowAndGamemodeIsA(){
@@ -70,7 +95,7 @@ public class Conecta4Tests {
     @Test
     public void testGameDoesNotFinishWhenFourInAHorizontalRowAndGamemodeIsB(){
         game = Conecta4B();
-        playAlternatively(game, new int[]{1, 1,2,2,3,3,4});
+        playAlternatively(game, new int[]{1,1,2,2,3,3,4});
         assertFalse(game.finished());
     }
 
@@ -89,16 +114,23 @@ public class Conecta4Tests {
     }
 
     @Test
-    public void testPuttingAPieceInAFullColumnShouldFail(){
-        playAlternatively(game, new int[]{1,1,1,1});
-        assertThrowsLike(() -> game.playRedAt(1), Conecta4.columnFull);
-        assertFalse(game.finished());
-    }
-
-    @Test
     public void testOnceTheGameIsFinishedPlayingAgainShouldFail(){
         playAlternatively(game, new int[]{1,2,2,3,3,4,3,4,1,4,4});
         assertThrowsLike(() -> game.playRedAt(1), GameStatus.gameOverMessage + " " + "Red won!");
+    }
+
+    private String emptyBoard() {
+        return
+                """
+                        +----+
+                        /    /
+                        /    /
+                        /    /
+                        /    /
+                        +----+
+                        Game Status: Red player's turn""";
+
+
     }
     private static Conecta4 Conecta4A() {
         return new Conecta4(4, 4, 'A');
